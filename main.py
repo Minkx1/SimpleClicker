9e35bf9c9bf7a9a2eb21944a5d6df5c24e2eea36
Minkx1 <zakalata.mark@gmail.com>
2025-08-15 21:22:01 +0300

    Improved max speed - now around 1000 cps. Changed Requirements

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 """===== main.py ====="""
 
-import pyautogui, time, threading
+import time, threading
 from pynput import keyboard
+from pynput.mouse import Controller,Button
+
 from settings import get_setting
 from UI import ClickerUI
 
 # Event to signal program stop
 stop_event = threading.Event()
+
+mouse = Controller()
+
+def str_to_btn(str):
+    if str=="left":
+        return Button.left
+    elif str=="right":
+        return Button.right
+    elif str=="middle":
+        return Button.middle
+    else:
+        return Button.left
 
 class Clicker():
     def __init__(self):
@@ -14,7 +28,7 @@ class Clicker():
         self.released_hold = False
         self.pause = True  # pause state
         self.mode = get_setting("clicker_mode") # clicker mode
-        self.button = get_setting("mouse_button") 
+        self.button = str_to_btn(get_setting("mouse_button"))
         self.click_cooldown = 1 / int(get_setting('cps')) # time between clicks
 
     def on_press(self, key):
@@ -44,25 +58,25 @@ class Clicker():
 
     def run(self):
         # Main clicker loop
-        
         while not stop_event.is_set():
             if not self.pause:  
                 if self.mode == "single":
-                    pyautogui.click(button=self.button)
+                    
+                    mouse.click(self.button)
                     time.sleep(self.click_cooldown)
+
                 elif self.mode == "double":
-                    pyautogui.click(button=self.button, clicks=2)
+
+                    mouse.click(self.button, 2)
                     time.sleep(self.click_cooldown)
+
                 elif self.mode == "hold":
                     if not self.hold:
-                        pyautogui.mouseDown(button=self.button)
+                        mouse.press(self.button)
                         self.hold = True
             else:
                 time.sleep(1.0)
-
-            self.mode = get_setting("clicker_mode") 
-            self.button = get_setting("mouse_button") 
-            self.click_cooldown = 1 / int(get_setting('cps')) 
+        mouse.release(self.button) 
 
 def start_ui():
     UI = ClickerUI()
